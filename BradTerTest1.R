b4dd9e44d80abeeb925f2d109fc969511e667a32
Merodoc <rowan@overworld.net.au>
2018-04-21 19:53:07 +1000

    testing

--- BradTerTest1.R
+++ BradTerTest1.R
@@ -2,35 +2,43 @@
 
 # Rowan Elson-Green - University of Queensland School of Mathematics and Physics
 
-BradTer <- function(outcome = 1, player1, player2, family = "binomial") {
-  call <- match.call()
-  
-  if (is.character(family))
-    family <- get(family, mode = "function", envir = parent.frame())
-  if (if.function(family))
-    family <- family()
-  if (is.null(family$family)) {
-    print(family)
-    stop("`family' not recognized")
+library("BradleyTerry2")
+
+FuncSum1 <- function(f, vector1) {
+  ans <- 0
+  for (val in vector1) {
+    ans = ans + f(val)
   }
-  if (family$family != "binomial")
-    stop("`family' must be binomial")
-  if(!family$link %in% c("logit", "probit", "cauchit"))
-    stop("link for binomial family must be one of \"logit\", \"probit\", or \"cauchit\"")
-  fcall <- as.list(match.call(expand.dots = FALSE))
-  setup <- match(c("outcome", "player1", "player2"), names(fcall), 0L)
-  
-  mf <- data.frame(X = setup$player1)
-  if (!is.null(setup$X)) { 
-    mf$X <- setup$X
-    formula <- Y ~ X - 1
+  print(ans)
+}
+
+
+
+BradTer <- function(outcome, player1, player2) {
+  # outcome <- matrix of win for/against outcomes
+  # player 1 and 2 corresponding to the winner/loser of outcome columns
+  K <- length(unique(player1))
+  K
+  ans1 <- c()
+  ans2 <- c()
+  p = c(1:K)
+  for (i in (1:K)) {
+    ans1 <-c(ans1, outcome[i,1]*log(p[i]))
+    print(ans1)
   }
-  else formula <- Y ~ 0
-  mf$Y <- setupY
-  
-  dotArgs <- fcall$"..."
-  if (is.null(setup$random)) {
-    method <
-    
+  for (i in (1:K)) {
+    if (outcome[i, 1] < outcome[i, 2]) 
+      ans2 <- c(ans2, sum(outcome[i,])*log(2* p[i]))
+      print(ans2)
   }
-  }
+  ans <- ans1 - ans2
+  print(ans)
+}
+
+data("citations", package = "BradleyTerry2")
+citations.sf <- countsToBinomial(citations)
+names(citations.sf)[1:2] <- c("journal1", "journal2")
+BradTer(cbind(citations.sf$win1,citations.sf$win2), citations.sf$journal1, citations.sf$journal2)
+
+BTm(cbind(win1, win2), journal1, journal2, data = citations.sf)
+
